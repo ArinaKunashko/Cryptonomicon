5eaaf316f46c9ea50b61fbe777ee14396ee170cc
Arina Kunashko <kunashko.arina@mail.ru>
2022-12-27 17:49:56 +0300

    add $emit on addButton

--- src/components/AddButton.vue
+++ src/components/AddButton.vue
@@ -1,5 +1,7 @@
 <template>
-    <button type="button" :disabled="disabled"
+    <button type="button" 
+    :disabled="disabled"
+    @click="$emit('click')"
         class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
         :class="{
             'opacity-50': disabled
@@ -26,5 +28,8 @@ export default {
             default: false
         }
     },
+    emits: {
+        click: null
+    }
 }
 </script>
